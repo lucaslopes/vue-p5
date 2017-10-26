1fb99ca2f9fef4cd5271b7ce029e42cd8b08aed5
Lucas Lopes <lucaslopesf2@gmail.com>
2017-10-26 00:42:33 -0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <p id="CanvasComponent">O canvas está aqui</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/CanvasComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="myCanvas">
+    <p>This is a p5 canvas</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CanvasComponent'
+}
+</script>
+
+<style scoped>
+</style>
